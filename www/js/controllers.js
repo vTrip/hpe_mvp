b6aaab7b4cbf943bc4d3d89713eb22e4e6b200ed
Vince Tripodi <vince@hellopraxis.com>
2016-04-22 10:10:34 +1000

    Styling to login input field

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,11 @@ angular.module('starter')
 
 .controller('LoginCtrl', function($scope, $state) {
 
+  $scope.$on("$ionicView.loaded", function(scopes, states, element) {
+    //focus on the input field once the view is loaded
+    element('form-login-password').focus();
+  });
+
   $scope.login = function login() {
     $state.go('menu');
   }
